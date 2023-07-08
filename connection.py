14f97698461610abd0d99d43292c91b736a2d564
aviiciii <laavesh1@gmail.com>
2023-07-08 23:17:34 +0530

    connection to mongodb

---
+++ connection.py
@@ -0,0 +1,9 @@
+
+from pymongo import MongoClient
+
+MONGO_DB_URI = 'mongodb+srv://myAtlasDBUser:<password>@myatlasclusteredu.h09az9g.mongodb.net/'
+
+client = MongoClient(MONGO_DB_URI)
+
+for db in client.list_databases():
+    print(db)
